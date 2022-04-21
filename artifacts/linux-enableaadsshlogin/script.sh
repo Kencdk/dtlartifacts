44b4629e4052220c425129cf8a50a3e67d5a8804
Ken Christensen <kenchr@microsoft.com>
2022-04-21 13:33:29 +0200

    Add new artifact linux-enableaadsshlogin

---
+++ artifacts/linux-enableaadsshlogin/script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+managed_identity="$1"
+virtualmachine_resourceid="$2"
+echo "Using $managed_identity"
+echo "Targeting $virtualmachine_resourceid"
+start_time=$(date +%s)
+echo "Current time : $(date)"
+az login --identity --username $managed_identity
+echo "Current time : $(date)"
+end_time=$(date +%s)
+elapsed=$(( end_time - start_time ))
+echo "Elapsed seconds: $elapsed"
+
+echo "Enable extension"
+
+start_time=$(date +%s)
+echo "Current time : $(date)"
+az vm extension set -n AADSSHLoginForLinux --publisher Microsoft.Azure.ActiveDirectory --ids $virtualmachine_resourceid
+echo "Current time : $(date)"
+end_time=$(date +%s)
+elapsed=$(( end_time - start_time ))
+echo "Elapsed seconds: $elapsed"
